78c297aa9ebf8e32063ad3677b893a951b596716
r3vn <giuseppe.corti@protonmail.com>
2020-01-15 11:09:11 +0100

    added evil twin attack

--- mount/desktop/__init__.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def check():
-	bashCommand = "echo testoutputbash"
-
-	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
-
-	return output
